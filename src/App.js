6c7305e8fe51d8bfff1a71d88d290b99256bce3c
ozdemirfadime <fadimeozdemir228@gmail.com>
2020-06-10 11:31:24 +0200

    changed branced

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         I was developing gradually with feeature branch :)
+         I was developing gradually with feature branch :)
         </a>
       </header>
     </div>
